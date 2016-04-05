7804a7112f2e567d622c53a1213bb0c00dd619b3
juliantamayo <julian-9419@hotmail.com>
2016-04-05 17:45:34 -0500

    vista billetera3

---
+++ application/views/website/ov/recargas/billetera.php
@@ -0,0 +1,42 @@
+
+<!-- MAIN CONTENT -->
+<div id="content">
+<div class="row">
+<div class="col-xs-12 col-sm-9 col-md-9 col-lg-9">
+<h1 class="page-title txt-color-blueDark">
+<a class="backHome" href="/bo"><i class="fa fa-home"></i> Menu</a>
+<span>> Billetera
+</span>
+</h1>
+</div>
+</div>
+<div class="well">
+<fieldset>
+<legend>Billetera</legend>
+<h5>Saldo Billetera Recargas</h5>
+<div class="col-lg-2 col-sm-4 col-md-4 col-xs-12">
+</div>
+<div class="col-lg-4 col-sm-4 col-md-4 col-xs-12">
+<a href="pedir_pago">
+<div class="well well-sm txt-color-white text-center link_dashboard" style="background:<?=$style[0]->btn_1_color?>">
+<i class="fa fa-money fa-3x"></i>
+<h5>Recargas GSM</h5>
+</div>
+</a>
+</div>
+<div class="well well-sm txt-color-white text-center link_dashboard" style="background:<?=$style[0]->btn_1_color?>">
+<i class="fa fa-money fa-3x"></i>
+<h5>Recargas VOIP</h5>
+</div>
+
+<div class="col-lg-4 col-sm-4 col-md-4 col-xs-12">
+<a href="/ov/billetera2/historial">
+<div class="well well-sm txt-color-white text-center link_dashboard" style="background:<?=$style[0]->btn_1_color?>">
+<i class="fa fa-calendar fa-3x"></i>
+<h5>TRANSFER</h5>
+</div>
+</a>
+</div>
+</fieldset>
+</div>
+</div>
