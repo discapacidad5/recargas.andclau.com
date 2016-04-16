f163b8f31d5f212c571846b2804d8dfcdd3c903d
juliantamayo <julian-9419@hotmail.com>
2016-04-16 11:52:43 -0500

    m

--- application/views/website/bo/recargas/editar_pin.php
+++ application/views/website/bo/recargas/editar_pin.php
@@ -4,7 +4,7 @@
 							<label class="input">
 								<input type="text" class="hide"  value="<?php echo $_POST['id']; ?>" name="id2">
 								<label class="input"> PIN
-								<input type="text" name="id" required placeholder="id" style="width: 50%;" class="form-control" value="<?php echo $pin[0]->id; ?>" required>
+								<input type="text" name="id" required placeholder="id" style="width: 50%;" size="10" class="form-control" value="<?php echo $pin[0]->id; ?>" required>
 								<label class="input"> Descripcion
 								<input type="text" name="descripcion" required placeholder="Descripcion" style="width: 50%;" class="form-control" value="<?php echo $pin[0]->descripcion; ?>" required>
 								<label class="input"> Valor
