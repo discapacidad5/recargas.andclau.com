4a21b10be9eaf3fef461714110be4d63b4bded02
qcmarcel <qc.marcel@gmail.com>
2016-04-25 19:26:10 -0500

    m

--- application/views/website/bo/comercial/altas/mercancias/membresia.php
+++ application/views/website/bo/comercial/altas/mercancias/membresia.php
@@ -102,7 +102,7 @@
 													<section class="col col-3">
 														<label class="input">
 														Puntos comisionables
-															<input type="number" min="1" max="" name="puntos_com" id="puntos_com">
+															<input type="number" min="0" max="" name="puntos_com" id="puntos_com">
 														</label>
 													</section>
 													<legend>Impuestos</legend>
